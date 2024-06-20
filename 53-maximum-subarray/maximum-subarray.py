a5a1cb81f2ef55d21f4f1c3972f545ee15e39d86
foramgandhicodecraft <foramgandhi2009@gmail.com>
2024-06-20 11:33:55 +0530

    Time: 540 ms (43.43%) | Memory: 24.7 MB (15.86%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        max_current = max_global = nums[0]
+
+        for i in range(1,len(nums)):
+            max_current = max(nums[i] , max_current+nums[i])
+
+            if (max_current > max_global):
+                max_global = max_current
+        
+        return max_global
+
+        
