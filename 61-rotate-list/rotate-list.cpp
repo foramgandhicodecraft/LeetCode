3962b4085e4d406bc09726f9b1c75ff766ada5ce
Foram Gandhi <foramgandhi2009@gmail.com>
2026-01-02 16:16:08 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (30.24%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,53 +11,29 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        //O(K*N) APPROACH
-        // if (head == NULL)
-        //   return NULL;
-        
-        // vector<int>v;
-        // ListNode* p =  head;
-        // while (p != NULL){
-        //     v.push_back(p->val);
-        //     p = p->next;
-        // }
-    
-        // while (k){
-        //     int temp = v[v.size()-1];
-        //     for (int i = v.size()-1; i > 0; i--){
-        //         v[i] = v[i-1];
-        //     }
-        //     v[0] = temp;
-        //     k--;
-        // }
-        // int i = 0;
-        // p = head;
-        // while (p != NULL){
-        //     p->val = v[i];
-        //     p = p->next;
-        //     i++;
-        // }
-        // return head;
-
-        if (head == NULL)
-          return NULL;
-
+        if (head == NULL) return NULL;
         ListNode* tail = head;
         int length = 1;
-        while (tail->next){
+
+        while (tail->next != nullptr){
             length++;
             tail = tail->next;
         }
-        k = k % length; //only do the necessary rotations, in multiples of length rotations not needed
-        tail->next = head;
-        int iterations = length - k - 1;
+
+        if (k%length == 0) return head;
+
+        k = k%length;
+        
+
         ListNode* p = head;
+        int iterations = length - k -1;
+        tail->next = head;
+
         while (iterations--){
             p = p->next;
         }
         head = p->next;
-
-        p->next = NULL; //watch striver 12 min video to understand
+        p->next = NULL;
 
         return head;
     }
