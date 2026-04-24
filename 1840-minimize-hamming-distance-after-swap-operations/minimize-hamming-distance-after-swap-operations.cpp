b653e28a9cd43dacef4f886c772174e6bd537543
Foram Gandhi <foramgandhi2009@gmail.com>
2026-04-24 16:46:59 +0530

    Time: 142 ms (50.02%) | Memory: 153.9 MB (39.24%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
     vector<int> parent;
+    vector<int>rank;
 
     int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
+        if (parent[x] == x){
+            return x;
+        }
+        return parent[x] = find(parent[x]);
     }
 
     void unite(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-        if (px != py) {
-            parent[py] = px;
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent){
+            return;
+        }
+
+        if (rank[x_parent] > rank[y_parent]){
+            parent[y_parent] = x_parent;
+        }
+        else if (rank[y_parent] > rank[x_parent]){
+            parent[x_parent] = y_parent;
+        }
+        else { 
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
         }
     }
 
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
         int n = source.size();
         parent.resize(n);
+        rank.resize(n);
 
         for (int i = 0; i < n; i++) parent[i] = i;
 
-        // build components
         for (auto &e : allowedSwaps) {
             unite(e[0], e[1]);
         }
