d8c39f07ee98d77c3dc9a90f5181ad2138c71773
Foram Gandhi <foramgandhi2009@gmail.com>
2026-05-19 00:44:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (31.98%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        int i = nums.size()-2;
 
-        while (i >= 0 && nums[i] >= nums[i+1]){
-            i -= 1;
+    void swap (int* a, int* b){
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = -1;
+
+        for (int i = n-2; i >= 0; i--){
+            if (nums[i] < nums[i+1]){
+                idx = i;
+                break;
+            }
         }
 
-        if (i == -1){
+        if (idx == -1){
             reverse(nums.begin(), nums.end());
             return;
         }
 
-        int j = nums.size() - 1;
-        while (nums[j] <= nums[i]){
-            j--;
-        }
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-
-        int left = i+1;
-        int right = nums.size() - 1;
-
-        while (left < right){
-            temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-            left++;
-            right--;
+        for (int i = n-1; i > idx; i--){
+            if (nums[i] > nums[idx]){
+                swap(&nums[i], &nums[idx]);
+                break;
+            }
         }
 
+        reverse(nums.begin()+idx+1, nums.end());
 
     }
 };
