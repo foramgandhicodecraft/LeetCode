df10bb747a2eda3d3827fe810190a3beee29c099
Foram Gandhi <foramgandhi2009@gmail.com>
2026-05-20 11:40:09 +0530

    Time: 8 ms (47.97%) | Memory: 88.6 MB (18.48%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -7,16 +7,13 @@ public:
         vector<int>ans;
 
         for (int i = 0; i < n; i++){
-            cout<<i<<endl;
             m[A[i]] += 1;
             m[B[i]] += 1;
             if (m[A[i]] > 1){
                 count++;
-                cout<<"count from A"<<count<<endl;
             }
             if ((m[B[i]] > 1) && (B[i] != A[i]) ){
                 count++;
-                cout<<"count from B"<<count<<endl;
             }
 
             ans.push_back(count);
