982b8a4dd2c70ff8526aef33ac48181b2765d6b4
Foram Gandhi <foramgandhi2009@gmail.com>
2026-05-22 13:11:57 +0530

    Time: 1747 ms (5.05%) | Memory: 91.2 MB (12.31%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()){
+        if (nums.size() == 0){
             return 0;
         }
-        int longest_streak = 0;
-        int current_streak = 0;
-        set<int>s(nums.begin(), nums.end());
-        int current_num = nums[0];
+        if (nums.size() == 1){
+            return 1;
+        }
+        sort(nums.begin(), nums.end());
+        int current_streak = 1;
+        int current_element = nums[0];
+        set<int>s;
+        s.insert(nums[0]);
+        int n = nums.size();
+        int max_streak = 0;
 
-        for (int num:s){
-            if (s.find(num-1) == s.end()){
-                current_streak = 1;
-                current_num = num;
-            }
-            while (s.find(current_num+1) != s.end()){
+        for (int i = 1; i < n; i++){
+            if (s.find(nums[i]-1) != s.end() && nums[i] != current_element){
                 current_streak++;
-                current_num++;
+                current_element = nums[i];
+                cout<<nums[i]<<":"<<current_streak<<endl;
+                s.insert(nums[i]);
             }
-
-            if (current_streak > longest_streak){
-                longest_streak = current_streak;
+            else {
+                if (nums[i] != current_element && i > 0){
+                    s.insert(nums[i]);
+                current_streak = 1;
+                current_element = nums[i];
+                cout<<nums[i]<<":"<<current_streak<<endl;
+                }
+                
             }
+            max_streak = max(max_streak, current_streak);
         }
 
-        return longest_streak;
+        return max_streak;
     }
 };
