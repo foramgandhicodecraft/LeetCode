2692510a173ce8b18f81bf2576a24e514bbbf332
Foram Gandhi <foramgandhi2009@gmail.com>
2026-05-24 11:31:36 +0530

    Time: 2362 ms (5.01%) | Memory: 598.5 MB (5.25%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,38 +2,41 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp (n, vector<int>(amount+1, -1));
-        int ans = f(coins, amount, n-1, dp);
+        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+        int ans = f(coins, dp, n-1, amount);
+
         if (ans >= 1e9){
             return -1;
-        }else{
-            return ans;
         }
-        return 0;
+
+        return ans;
     }
 
-    int f(vector<int>&nums, int target, int index, vector<vector<int>>& dp){
-        if (index == 0){
-            if (target % nums[0] == 0){ // calculate how many nums[0] will be needed
-                return target / nums[0];
-            } else{
-                return 1e9; // some large value
+    int f (vector<int>nums, vector<vector<int>>&dp, int idx, int target){
+        if (idx == 0){
+            if (target % nums[0] == 0){
+                return target/nums[0];
+            }
+            else {
+                return 1e9;
             }
         }
 
-        if (target == 0) // when target is reached no further coins are requires
-          return 0;
-
-        if (dp[index][target] != -1)
-          return dp[index][target];
-
-        int not_take = f(nums, target, index-1, dp);
-        int take = 1e9;
-
-        if (nums[index] <= target){
-            take = 1 + f(nums, target-nums[index], index, dp);
+        if (target == 0){
+            return 0;
         }
 
-        return dp[index][target] = min(take,not_take);
+        if (dp[idx][target] != -1){
+            return dp[idx][target];
+        }
+
+        int not_take = f(nums, dp, idx-1, target);
+        int take = 1e9;
+
+        if (nums[idx] <= target){
+            take = 1 + f(nums, dp, idx, target-nums[idx]);
+        }
+
+        return dp[idx][target] = min(take, not_take);
     }
 };
