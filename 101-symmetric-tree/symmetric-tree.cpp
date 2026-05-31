4ff88bc1c80776de914e998fc2fa83e44ee0df88
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-01 00:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (58.77%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -12,45 +12,28 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        // vector<int>arr;
-        // inorder(root,arr);
-
-        // int left = 0;
-        // int right = arr.size()-1;
-        // for (int num:arr){
-        //     cout<<num;
-        // }
-
-        // while (left < right){
-        //     if (arr[left] != arr[right]){
-        //         return false;
-        //     }
-        //     left++;
-        //     right--;
-        // }
-
-        // return true;
-
-        if (!root) return true;
+        if (!root){
+            return true;
+        }
 
         return f(root->left, root->right);
-
-        
     }
 
-    // void inorder(TreeNode* root, vector<int>&arr){
-    //     if (root){
-    //         inorder(root->left, arr);
-    //         arr.push_back(root->val);
-    //         inorder(root->right, arr);
-    //     }
-    // }
+    bool f(TreeNode* t1, TreeNode* t2) {
+        if (!t1 && !t2){
+            return true;
+        }
 
-    bool f(TreeNode* t1, TreeNode* t2){
-        if (!t1 && !t2) return true;
+        if (!t1 || !t2){
+            return false;
+        }
+        if (t1->val != t2->val){
+            return false;
+        }
 
-        if (!t1 || !t2) return false;
+        bool left = f(t1->left, t2->right);
+        bool right = f(t1->right, t2->left);
 
-        return (t1->val == t2->val) && f(t1->left, t2->right) && f(t1->right, t2->left);
+        return left && right;
     }
 };
