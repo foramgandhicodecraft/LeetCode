110eee834a54e4b73274b4f79980d47edb84dab3
Foram Gandhi <foramgandhi2009@gmail.com>
2026-05-31 20:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (12.56%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,48 +12,46 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p==NULL && q== NULL)
-          return true;
-        if (p==NULL && q!=NULL)
-          return false;
-        if (q==NULL && p!=NULL)
-          return false;
-
+        if (p == nullptr && q == nullptr){
+            return true;
+        }
+        if (p == nullptr || q == nullptr){
+            return false;
+        }
         queue<TreeNode*>q1;
         queue<TreeNode*>q2;
         q1.push(p);
         q2.push(q);
-        bool ans = bfs(p,q,q1,q2);
-        return ans;
+        return bfs(p,q,q1,q2);
     }
 
-private:
-    bool bfs(TreeNode* p, TreeNode* q, queue<TreeNode*>q1, queue<TreeNode*>q2){
+    bool bfs (TreeNode* p, TreeNode* q, queue<TreeNode*>q1, queue<TreeNode*>q2){
         while (!q1.empty() && !q2.empty()){
             TreeNode* c1 = q1.front();
             TreeNode* c2 = q2.front();
             q1.pop();
             q2.pop();
 
-            //both nodes null continue
-            if (!c1 && !c2)
-              continue;
-             //one node null return false
-            if (!c1 || !c2)
-              return false;
-              
-
-            if (c1->val != c2->val)
-              return false;
+            if (!c1 && !c2){
+                continue;
+            }
+            if (!c1 || !c2){
+                return false;
+            }
+            if (c1->val != c2->val){
+                return false;
+            }
 
             q1.push(c1->left);
             q1.push(c1->right);
             q2.push(c2->left);
             q2.push(c2->right);
- 
         }
-        if (!q1.empty() || !q2.empty())
-          return false;
+
+        if (!q1.empty() || !q2.empty()){
+            return false;
+        }
+
         return true;
     }
 };
