5fddf58f8992a81460147b7fa1e0fcaad9302f2f
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-01 12:20:15 +0530

    Time: 2 ms (41.74%) | Memory: 10.7 MB (42.28%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,29 +2,33 @@ class Solution {
 public:
     string reverseWords(string s) {
         stack<string>st;
+        string ans;
         string current;
+
         for (int i = 0; i < s.size(); i++){
-            if (s[i] != ' ')
-                current.push_back(s[i]);
+            if (s[i] != ' '){
+                current += s[i];
+            }
             else{
-                if (!current.empty()){
-                 st.push(current);
-                 current = "";
+                if (!(current.size() == 0)) {
+                    st.push(current);
+                    current = "";
                 }
-            }   
+            }
         }
+
         if (!current.empty()){
             st.push(current);
         }
-        string ans;
-        string word;
+
         while (!st.empty()){
-            word = st.top();
-            st.pop();
+            string word = st.top();
             ans += word;
+            st.pop();
             ans += " ";
         }
-        ans.erase(ans.end()-1);
+        ans.erase(ans.begin()+(ans.size()-1));
+
         return ans;
     }
 };
