2fd8e9a6f651b0afaa699ec54a0d6dbf0218cd31
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-03 01:38:31 +0530

    Time: 11 ms (40.16%) | Memory: 91.7 MB (99.18%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,59 +1,38 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& a, vector<int>& b, vector<int>& c, vector<int>& d) {
-        // int min_add = INT_MAX;
-        // int min_land = INT_MAX;
-        // int min_water = INT_MAX;
-        // int from_land = true;
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int min_time = INT_MAX;
+        int land = landStartTime.size();
+        int water = waterStartTime.size();
 
-        // for (int i = 0; i < landStartTime.size(); i++){
-        //     min_add = min(min_add, landStartTime[i] + landDuration[i]);
-        //     min_land = min(min_land, landDuration[i]);
-        // }
-
-        // for (int i = 0; i < waterStartTime.size(); i++){
-        //     if (waterStartTime[i] + waterDuration[i] < min_add){
-        //         min_add = waterStartTime[i] + waterDuration[i];
-        //         from_land = false;
-        //     }
-        //     min_water = min(min_water, waterDuration[i]);
-        // }
-
-        // int ans;
-
-        // if (from_land){
-        //     ans = min_add + min_water;
-        // } else {
-        //     ans = min_add + min_land;
-        // }
-
-        // return ans;
-
-        int land = INT_MAX;
-        int water = INT_MAX;
-
-        for (int i = 0; i < a.size(); i++){
-            land = min(land, a[i] + b[i]);
+        for (int i = 0; i < land; i++){
+            int current_land = landStartTime[i] + landDuration[i];
+            for (int j = 0; j < water; j++){
+                if (waterStartTime[j] > current_land) {
+                    int total_time = current_land +  waterDuration[j] + (waterStartTime[j] - current_land);
+                    min_time = min(min_time, total_time);
+                }
+                else {
+                    int total_time = current_land + waterDuration[j];
+                    min_time = min(min_time, total_time);
+                }
+            }
         }
 
-        for (int i = 0; i < c.size(); i++){
-            water = min(water, c[i]+d[i]);
+        for (int i = 0; i < water; i++){
+            int current_water = waterStartTime[i] + waterDuration[i];
+            for (int j = 0; j < land; j++){
+                if (landStartTime[j] > current_water) {
+                    int total_time = current_water +landDuration[j] + (landStartTime[j] - current_water);
+                    min_time = min(min_time, total_time);
+                }
+                else {
+                    int total_time = current_water + landDuration[j];
+                    min_time = min(min_time, total_time);
+                }
+            }
         }
 
-
-        int land_water = INT_MAX;
-        for (int i = 0; i < c.size(); i++){
-            land_water = min(land_water, max(land, c[i])+d[i]);
-        }
-
-        int water_land = INT_MAX;
-        for (int i = 0; i < a.size(); i++){
-            water_land = min(water_land, max(water, a[i])+b[i]);
-        }
-
-        int ans = min(land_water, water_land);
-
-        return ans;
-        
+        return min_time;
     }
 };
