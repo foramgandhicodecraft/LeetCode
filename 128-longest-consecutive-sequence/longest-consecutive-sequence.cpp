9b5a82ed5dec4396748a758ea2adb80466414203
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-08 12:35:59 +0530

    Time: 159 ms (8.03%) | Memory: 91.2 MB (14.08%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,34 +4,22 @@ public:
         if (nums.size() == 0){
             return 0;
         }
-        if (nums.size() == 1){
-            return 1;
-        }
-        sort(nums.begin(), nums.end());
-        int current_streak = 1;
-        int current_element = nums[0];
         set<int>s;
-        s.insert(nums[0]);
-        int n = nums.size();
-        int max_streak = 0;
+        int max_streak = 1;
+        int current_streak = 1;
 
-        for (int i = 1; i < n; i++){
-            if (s.find(nums[i]-1) != s.end() && nums[i] != current_element){
+        for (int num:nums){
+            s.insert(num);
+        }
+
+        for (int num:s){
+            if (s.find(num+1) != s.end()){
                 current_streak++;
-                current_element = nums[i];
-                cout<<nums[i]<<":"<<current_streak<<endl;
-                s.insert(nums[i]);
+                max_streak = max(max_streak, current_streak);
             }
             else {
-                if (nums[i] != current_element && i > 0){
-                    s.insert(nums[i]);
                 current_streak = 1;
-                current_element = nums[i];
-                cout<<nums[i]<<":"<<current_streak<<endl;
-                }
-                
             }
-            max_streak = max(max_streak, current_streak);
         }
 
         return max_streak;
