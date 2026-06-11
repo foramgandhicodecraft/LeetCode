e4d0b6decf897aa982506c38ccfce3d65c60f235
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-11 16:45:32 +0530

    Time: 0 ms (100.00%) | Memory: 77.4 MB (12.03%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -12,29 +12,42 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int carry = 0;
-        ListNode dummy(0);
-        ListNode *current = &dummy;
+        ListNode dummy;
+        ListNode* prev = &dummy;
 
-        while (l1 != nullptr || l2 != nullptr){
-            int sum = carry;
-            if (l1 != nullptr){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != nullptr){
-                sum += l2->val;
-                l2 = l2->next;
-            }
+        while (l1 != nullptr && l2 != nullptr) {
+            int sum = l1->val + l2->val+carry;
+            ListNode* current = new ListNode(sum%10);
+            prev->next = current;
+            prev = current;
             carry = sum/10;
-            current->next = new ListNode(sum%10);
-            current = current->next;
+            l1 = l1->next;
+            l2 = l2->next;
         }
 
-        if (carry > 0){
-            current->next = new ListNode(carry);
+        while (l1 != nullptr){
+            int sum = l1->val+carry;
+            ListNode* current = new ListNode(sum%10);
+            prev->next = current;
+            prev = current;
+            carry = sum/10;
+            l1 = l1->next;
+        }
+
+        while (l2 != nullptr){
+            int sum = l2->val+carry;
+            ListNode* current = new ListNode(sum%10);
+            prev->next = current;
+            prev = current;
+            carry = sum/10;
+            l2 = l2->next;
+        }
+
+        if (carry != 0){
+            ListNode* current = new ListNode(carry);
+            prev->next = current;
         }
 
         return dummy.next;
-        
     }
 };
