effa8ba3454691a7b063f6a149d5e70d220f9762
Foram Gandhi <foramgandhi2009@gmail.com>
2026-06-11 22:40:37 +0530

    Time: 14 ms (1.75%) | Memory: 22.3 MB (7.96%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -6,34 +6,37 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        int prev = INT_MIN;
-        vector<int>arr;
-        inorder(root, arr);
 
-        for (int i = 1; i < arr.size(); i++){
-            if (arr[i] <= arr[i-1])
-             return false;
+        vector<int>inorder;
+        construct(inorder, root);
+
+        for (int i = 0; i < inorder.size(); i++){
+            cout<<inorder[i];
+        }
+
+        for (int i = 0; i < inorder.size()-1; i++){
+            if (inorder[i] >= inorder[i+1]){
+                return false;
+            }
         }
 
         return true;
+        
     }
 
-    void inorder(TreeNode* root, vector<int>&arr) {
-        if (root) {
+    void construct(vector<int>&inorder, TreeNode* root){
+        if (root == nullptr) return;
 
-            inorder(root->left, arr);
-
-            arr.push_back(root->val);
-
-            inorder(root->right, arr);
-        }
+        construct(inorder, root->left);
+        inorder.push_back(root->val);
+        construct(inorder, root->right);
+        //inorder.push_back(root->val);
 
     }
 };
